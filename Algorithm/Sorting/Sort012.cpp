1eadebed28b7c781ee3bf770e6d8b140ce09d510
braham517 <47574172+braham517@users.noreply.github.com>
2020-06-29 13:13:58 +0530

    Create Sort012.cpp

---
+++ Algorithm/Sorting/Sort012.cpp
@@ -0,0 +1,22 @@
+void sort012(int arr[], int n)
+{
+    int l=0,m=0,h=n-1;
+    int i=0;
+    while(m<=h)
+    {
+        if(arr[m]==0)
+        {
+            swap(arr[m],arr[l]);
+            l++;
+            m++;
+        }
+        else if(arr[m]==1)
+        m++;
+        else if(arr[m]==2)
+        {
+            swap(arr[m],arr[h]);
+            h--;
+            
+        }
+    }
+}

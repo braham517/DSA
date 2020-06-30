a2e01b6f1b95fba6d28e50d26e77695a098cbb70
braham517 <47574172+braham517@users.noreply.github.com>
2020-06-30 10:44:24 +0530

    Create BottomView.cpp

---
+++ Data Structure/Tree/Views/BottomView.cpp
@@ -0,0 +1,26 @@
+void bottomView(Node *root)
+{
+  if(root==NULL)
+    return;
+    int d=0;
+    map<int,vector<int>> m;
+    queue<pair<Node*,int>> q;
+    
+    q.push(make_pair(root,d));
+    while(!q.empty()){
+        pair<Node*,int> temp=q.front();
+        q.pop();
+         d=temp.second;
+        Node* a=temp.first;
+        m[d].push_back(a->data);
+        if(a->left)
+        q.push(make_pair(a->left,d-1));
+        if(a->right)
+        q.push(make_pair(a->right,d+1));
+    }
+    for(auto i:m)
+    {
+        cout<<i.second[i.second.size()-1]<<" ";
+    }
+}
+

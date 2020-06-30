f372292bea59f5f78eb8931ac0e0cd117b68e99d
braham517 <47574172+braham517@users.noreply.github.com>
2020-06-30 10:37:46 +0530

    Create LeftView.cpp

---
+++ Data Structure/Tree/Views/LeftView.cpp
@@ -0,0 +1,26 @@
+void leftView(Node* root)
+{
+
+    if (root == NULL)
+        return;
+    queue<Node*> q;
+    q.push(root);
+    Node* curr = NULL;
+    while (!q.empty())
+    {
+        int size = q.size();
+        int i = 0;
+        while (i < size)
+        {   
+            curr = q.front();
+            q.pop();
+           if (i == 0)
+                cout<<curr->data<< " ";
+           if (curr->left)
+                q.push(curr->left);
+           if (curr->right)
+                q.push(curr->right);
+            i++;
+        }
+    }
+}

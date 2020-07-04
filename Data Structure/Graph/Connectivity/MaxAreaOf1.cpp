22b5bd27c2441bf59b39720d49e354b637609bd6
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-04 11:20:16 +0530

    Create MaxAreaOf1.cpp

---
+++ Data Structure/Graph/Connectivity/MaxAreaOf1.cpp
@@ -0,0 +1,37 @@
+vector<int> v1={1,-1,0,0,1,1,-1,-1};
+vector<int> v2={0,0,1,-1,1,-1,1,-1};
+int issafe(int i,int j,int n,int m){
+    if(i<0||j<0||i>=n||j>=m){
+        return 0;
+    }
+    return 1;
+}
+
+void dfs(vector<vector<int> > &A,vector<vector<int>> &vis,int i,int j,int N,int M,&ans){
+    ans++;
+    vis[i][j]=1;
+    for(int k=0;k<8;k++){
+        int u=i+v1[k];int v=j+v2[k];
+        if(issafe(u,v,N,M)&&vis[u][v]==0&&A[u][v]){
+            dfs(A,vis,u,v,N,M,ans);
+        }
+    }
+}
+int Solution::solve(vector<vector<int> > &A) {
+
+int N=A.size();
+int M=A[0].size();
+vector<vector<int>> vis(N,vector<int> (M,0));
+int a,b;
+int f=INT_MIN;
+for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(!vis[i][j]&&A[i][j]){
+                int ans=0;
+                dfs(A,vis,i,j,N,M,ans);
+                f=max(ans,f);
+            }
+        }
+    }
+    return f;
+}

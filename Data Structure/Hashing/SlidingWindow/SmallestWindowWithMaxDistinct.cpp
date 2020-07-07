41d8eace35ba2e672f1e8e0efdbb6abc081c4901
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-07 16:30:16 +0530

    Create SmallestWindowWithMaxDistinct.cpp

---
+++ Data Structure/Hashing/SlidingWindow/SmallestWindowWithMaxDistinct.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    cin>>s;
+	    map<char,int> m;
+	    int x[26]={0};
+	    for(int i=0;i<s.length();i++)
+	    m[s[i]]++;
+	    int k=m.size();
+	    int st=0;
+	    int minl=INT_MAX;
+	    int dis=0;
+	    for(int i=0;i<s.length();i++)
+	    {
+	        if(x[s[i]-'a']==0)
+	        dis++;
+	        x[s[i]-'a']++;
+	        if(dis==k)
+	        {
+	           while(x[s[st]-'a']>1){
+	               x[s[st]-'a']--;
+	               st++;
+	        }
+	        if(i-st+1<minl)
+	         minl=i-st+1;
+	         }
+	    }
+	    cout<<minl<<endl;
+	}
+	return 0;
+}

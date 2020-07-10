7178c3e31d1a3118efc93b90cb10d8222bb7fbfa
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-10 23:17:21 +0530

    Update SubsetSum.cpp

--- Algorithm/Dynamic Programming/KnapSack/SubsetSum.cpp
+++ Algorithm/Dynamic Programming/KnapSack/SubsetSum.cpp
@@ -1,46 +1,23 @@
-
-int main() {
-	//code
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin>>n;
-	    int arr[n];
-	    int w=0;
-	    for(int i=0;i<n;i++){
-	    cin>>arr[i];
-	    w+=arr[i];
-	    }
-	    int flag=0;
-    if(w%2!=0){
-    cout<<"NO"<<endl;
-    }
-    else{
-	    w=w/2;
-	  bool t[n+1][w+1];
+void equal(int arr[],int n,int sum)
+{
+    bool t[n+1][sum+1];
+    for(int i=0;i<=sum;i++)
+    t[0][i]=false;
     for(int i=0;i<=n;i++)
+    t[i][0]=true;
+    for(int i=1;i<=n;i++)
     {
-        for(int j=0;j<=w;j++)
+        for(int j=1;j<=sum;j++)
         {
-            if(i==0)
-            t[i][j]=false;
-            else if(j==0)
-            t[i][j]=true;
-            else if(arr[i-1]<=j)
-            {
-                t[i][j]=t[i-1][j]||t[i-1][j-arr[i-1]];
-            }
+            if(arr[i-1]<=j)
+            t[i][j]=t[i-1][j]||t[i-1][j-arr[i-1]];
             else
             t[i][j]=t[i-1][j];
-        }        
+        }
     }
-    if(t[n][w]==true)
+    if(t[n][sum])
     cout<<"YES"<<endl;
     else
     cout<<"NO"<<endl;
-}
-	}
-	return 0;
+    
 }

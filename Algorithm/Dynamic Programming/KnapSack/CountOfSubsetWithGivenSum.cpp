83e5d146dbd719b46a6120d2779c537e6ef6c92f
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-10 23:36:06 +0530

    Update CountOfSubsetWithGivenSum.cpp

--- Algorithm/Dynamic Programming/KnapSack/CountOfSubsetWithGivenSum.cpp
+++ Algorithm/Dynamic Programming/KnapSack/CountOfSubsetWithGivenSum.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 int count(int arr[],int n,int sum)
 {
     int t[n+1][sum+1];
@@ -10,10 +12,26 @@ int count(int arr[],int n,int sum)
         for(int j=1;j<=sum;j++)
         {
             if(arr[i-1]<=j)
-            t[i][j]=t[i-1][j]+t[i-1][j-arr[i-1]];
+            t[i][j]=(t[i-1][j]+t[i-1][j-arr[i-1]])%1000000007;
             else
-            t[i][j]=t[i-1][j];
+            t[i][j]=t[i-1][j]%1000000007;
         }
     }
-  return t[n][sum];    
+  return t[n][sum]%1000000007;    
+}
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    int sum;
+	    cin>>sum;
+	    cout<<count(arr,n,sum)<<endl;
+	}
+	return 0;
 }

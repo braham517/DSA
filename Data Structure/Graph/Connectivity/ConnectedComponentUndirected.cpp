20e9d99ed32ba833ea0e9a32815b1cd82b827b4e
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-11 11:58:25 +0530

    Update ConnectedComponentUndirected.cpp

--- Data Structure/Graph/Connectivity/ConnectedComponentUndirected.cpp
+++ Data Structure/Graph/Connectivity/ConnectedComponentUndirected.cpp
@@ -1,18 +1,38 @@
-void Graph::connectedComponents() 
-{ 
-    
-    bool *visited = new bool[V]; 
-    for(int v = 0; v < V; v++) 
-        visited[v] = false; 
-  
-    for (int v=0; v<V; v++) 
-    { 
-        if (visited[v] == false) 
-        {
-            DFSUtil(v, visited); 
-  
-            cout << "\n"; 
-        } 
-    } 
-    delete[] visited; 
-} 
+#include<bits/stdc++.h>
+using namespace std;
+void dfs(vector<int> adj[],int vis[],int s,vector<int> &v)
+{
+    vis[s]=1;
+    v.push_back(s);
+    for(int i=0;i<adj[s].size();i++)
+    {
+        if(vis[adj[s][i]]==0)
+            dfs(adj,vis,adj[s][i],v);
+    }
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[n];
+    for(int i=0;i<m;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    int vis[n]={0};
+     vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        if(vis[i]==0){
+       
+        dfs(adj,vis,i,v);
+        sort(v.begin(),v.end());
+        for(int i=0;i<v.size();i++)
+          cout<<v[i]<<" ";
+            v.clear();
+        cout<<endl;
+        }
+    }
+}

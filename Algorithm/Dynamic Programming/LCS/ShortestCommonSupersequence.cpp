de5d495a2278ef44d1e407a62a7278eb0330b35a
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-13 12:04:45 +0530

    Create ShortestCommonSupersequence.cpp

---
+++ Algorithm/Dynamic Programming/LCS/ShortestCommonSupersequence.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+int lcs(int n,int m,string a,string b){
+int t[n+1][m+1];
+for(int i=0;i<=n;i++)
+{
+    for(int j=0;j<=m;j++)
+    {
+        if(i==0||j==0)
+        t[i][j]=0;
+        else if(a[i-1]==b[j-1])
+        {
+            t[i][j]=1+t[i-1][j-1];
+        }
+        else{
+            t[i][j]=max(t[i-1][j],t[i][j-1]);
+        }
+    }
+}
+
+return n+m-t[n][m];
+}
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+
+	    string a,b;
+	    cin>>a>>b;
+	    cout<<lcs(a.length(),b.length(),a,b)<<endl;;
+	}
+	return 0;
+}

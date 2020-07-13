01ba2b0ade1363fcb2af3e673161e2ab06f00b9e
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-13 12:09:30 +0530

    Create LongestPalindromeSubsequence.cpp

---
+++ Algorithm/Dynamic Programming/LCS/LongestPalindromeSubsequence.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lcs(int n,int m,string a,string b){
+int t[n+1][m+1];
+for(int i=0;i<=n;i++)
+{
+    for(int j=0;j<=m;j++)
+    {
+        if(i==0||j==0)
+        t[i][j]=0;
+        else if(a[i-1]==b[j-1])
+        {
+            t[i][j]=1+t[i-1][j-1];
+        }
+        else{
+            t[i][j]=max(t[i-1][j],t[i][j-1]);
+        }
+    }
+}
+return t[n][m];
+}
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    string a,b;
+	    cin>>a;
+      b=reverse(a.begin(),e.end());
+	    cout<<lcs(n,n,a,b)<<endl;;
+	}
+	return 0;
+}

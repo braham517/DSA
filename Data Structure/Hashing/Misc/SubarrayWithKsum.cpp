358336130641b8e36892772e687734ace72c0313
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-14 12:01:41 +0530

    Create SubarrayWithKsum.cpp

---
+++ Data Structure/Hashing/Misc/SubarrayWithKsum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n;
+	    int arr[n];
+	    map<int,int> m;
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    int sum=0;
+	    int f=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        sum+=arr[i];
+	        if(m.find(sum-k)!=m.end()||sum==k)
+	        {
+	            f=1;
+	            cout<<"Yes"<<endl;
+	            break;
+	        }
+	        m[sum]++;
+	    
+	    }
+	    if(f==0)
+	    cout<<"No"<<endl;
+	    m.clear();
+	}
+	return 0;
+}

46a53396de37f0fa9ad590287480cc0a93d978fd
braham517 <47574172+braham517@users.noreply.github.com>
2020-07-16 11:27:11 +0530

    Create AnagramSearch.cpp

---
+++ Data Structure/String/Anagram/AnagramSearch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+int main()
+{
+int num;
+cin>>num;
+while(num--)
+{
+string str;
+string text;
+cin>>str;
+cin>>text;
+sort(text.begin(),text.end());
+int count=0;
+for(int i=0;i<=str.length()-text.length();i++)
+{
+string s=str.substr(i,text.length());
+sort(s.begin(),s.end());
+if(s==text)
+count++;
+}
+cout<<count<<endl; } }

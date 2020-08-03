f264d4f3bdcaf724a38e51e667cc548cba37324a
braham517 <47574172+braham517@users.noreply.github.com>
2020-08-03 10:27:36 +0530

    Create 4. MaxConsecutive1.cpp

---
+++ Data Structure/Array/Basic/4. MaxConsecutive1.cpp
@@ -0,0 +1,16 @@
+int getMaxLength(bool arr[], int n) 
+{ 
+    int count = 0; 
+    int result = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        if (arr[i] == 0) 
+            count = 0;
+        else
+        { 
+            count++;
+            result = max(result, count); 
+        } 
+    } 
+    return result; 
+} 

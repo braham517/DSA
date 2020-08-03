a3c7ae191af5a122dbf035baa59261426939fd89
braham517 <47574172+braham517@users.noreply.github.com>
2020-08-03 15:36:40 +0530

    Create 4. BuidingBriges.cpp

---
+++ Algorithm/Dynamic Programming/LIS/4. BuidingBriges.cpp
@@ -0,0 +1,42 @@
+struct CityPairs 
+{ 
+    int north, south; 
+}; 
+  
+// comparison function to sort 
+// the given set of CityPairs 
+bool compare(struct CityPairs a, struct CityPairs b) 
+{ 
+    if (a.south == b.south) 
+        return a.north < b.north; 
+    return a.south < b.south; 
+} 
+  
+// function to find the maximum number 
+// of bridges that can be built 
+int maxBridges(struct CityPairs values[], int n) 
+{ 
+    int lis[n]; 
+    for (int i=0; i<n; i++) 
+        lis[i] = 1; 
+          
+    sort(values, values+n, compare); 
+      
+    // logic of longest increasing subsequence 
+    // applied on the northern coordinates 
+    for (int i=1; i<n; i++) 
+        for (int j=0; j<i; j++) 
+            if (values[i].north >= values[j].north  
+                && lis[i] < 1 + lis[j]) 
+                lis[i] = 1 + lis[j]; 
+          
+          
+    int max = lis[0]; 
+    for (int i=1; i<n; i++) 
+        if (max < lis[i]) 
+            max = lis[i]; 
+      
+    // required number of bridges 
+    // that can be built         
+    return max;         
+} 
